107d415d84ed22488ddb19fe36392a80b96dcbad
ShashwatiVatsa <99708006+ShashwatiVatsa@users.noreply.github.com>
2026-04-25 14:04:09 +0530

    Rename uploaded model file to best_prediction_model_v1
    updated train.py file

--- mlops/model_building/train.py
+++ mlops/model_building/train.py
@@ -162,7 +162,7 @@ with mlflow.start_run():
     # create_repo("churn-model", repo_type="model", private=False)
     api.upload_file(
         path_or_fileobj="best_prediction_model_v1.joblib",
-        path_in_repo="best_churn_model_v1.joblib",
+        path_in_repo="best_prediction_model_v1.joblib",
         repo_id=repo_id,
         repo_type=repo_type,
     )
